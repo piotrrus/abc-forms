d171eb4c3f0067486f7dac4044d98bfc89ceedb7
piotrrus <irache@wp.pl>
2025-01-28 14:35:47 +0100

    Add files via upload

---
+++ src/app/features/incident-printing/enums/block-styles.enum.ts
@@ -0,0 +1,12 @@
+export enum BLOCK_STYLES {
+    HEADER = 'header',
+    SUBHEADER = 'subheader',
+    LIST_TITLE = 'listTitle',
+    LIST = 'list',
+    SECTION_ITEM = 'sectionItem',
+    SECTION_HEADER = 'sectionHeader',
+    DESCRIPTION = 'description',
+    FOOOTER = 'footer',
+
+
+}
