f7f6f89f2018e3e98da3b71334289ec02c410230
piotrrek <irache@wp.pl>
2025-01-29 12:05:52 +0100

    incident-types services  data

---
+++ src/app/features/incident-registration-page/services/incident-types.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { catchError, Observable, shareReplay, take, throwError } from 'rxjs';
+import { List } from '@features/incident-registration-page/models/list.interface';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { COMMON_MESSAGES } from 'src/shared/enums/common-messages';
+
+@Injectable()
+export class IncidentTypesService {
+     constructor(private http: HttpClient) {}
+
+     public getImpactList(): Observable<List[]> {
+          return this.http.get<List[]>('assets/data/incident-types.json').pipe(
+               shareReplay(1),
+               take(1),
+               catchError((error: HttpErrorResponse) => {
+                    console.log(`${COMMON_MESSAGES.ERROR_OCCURED} ${error.message}`);
+                    return throwError(() => error);
+               })
+          );
+     }
+}
