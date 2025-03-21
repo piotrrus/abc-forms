551701c8cd117cc60b60a5f13bd25c8004560bb8
piotrrus <irache@wp.pl>
2025-03-21 06:37:15 +0100

    Add files via upload

---
+++ src/app/features/incident-registration-page/uodo-state/uodo.state.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { State } from '@shared/state/state';
+import { map, Observable } from 'rxjs';
+import { DescriptionDTO } from 'src/description-dto';
+
+export const DEFAULT_UODO_STATE = (): any => ({
+     descriptionDTO: <DescriptionDTO>{},
+});
+
+@Injectable()
+export class UodoState extends State<any> {
+     constructor() {
+          super(DEFAULT_UODO_STATE());
+     }
+
+     public patchUodoForm(value: any): void {
+          this.patchState(value);
+     }
+
+     public getUodoValue(): Observable<any> {
+          return this.state$.pipe(map((state: any) => state));
+     }
+
+     public patchSection1(value: any): void {
+          this.patchState(value);
+     }
+}
