f5b6822ff4a6acc3c3be20077a79f117e060dfb8
piotrrus <irache@wp.pl>
2025-03-27 13:26:52 +0100

    Add files via upload

--- trim.directive.ts
+++ trim.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-     selector: '[appTrim]',
+     selector: '[bldTrim]',
 })
 export class TrimDirective {
      constructor(private el: ElementRef<HTMLInputElement>) {}
